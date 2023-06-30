0f8d612c5d14fd81d0b29e2b08ffbb7de752c01d
alandlan <alanhorcrux@gmail.com>
2023-06-30 17:33:14 -0300

    configurando layout de rotas

--- ignite-timer/src/Router.tsx
+++ ignite-timer/src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { DefaultLayout } from './layout/DefaultLayout';
 
 export function Router() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="*" element={<h1>Not Found</h1>} />
-			<Route path="/history" element={<h1>History</h1>} />
+			<Route path="/" element={<DefaultLayout />}>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<h1>Not Found</h1>} />
+				<Route path="/history" element={<h1>History</h1>} />
+			</Route>
 		</Routes>
 	);
 }
