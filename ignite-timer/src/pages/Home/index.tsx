16e27bdabe2405ec0b675a07366607d8518b34ea
alandlan <alanhorcrux@gmail.com>
2023-07-12 17:03:51 -0300

    adiciona form home

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,20 +1,47 @@
 import { Play } from 'phosphor-react';
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles';
+import {
+    CountdownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountdownButton,
+    TaskInput
+} from './styles';
+import { useState } from 'react';
 
 export function Home() {
+    const [task, setTask] = useState('');
+
     return (
         <HomeContainer>
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em </label>
-                    <TaskInput type="text" list="tasks" name="task" id="task" placeholder="De um nome para o seu projeto" />
+                    <TaskInput
+                        type="text"
+                        list="tasks"
+                        name="task"
+                        id="task"
+                        placeholder="De um nome para o seu projeto"
+                        onChange={(e) => setTask(e.target.value)}
+                        value={task}
+                    />
                     <datalist id="tasks">
                         <option value="Projeto 1" />
                         <option value="Projeto 11" />
                         <option value="Projeto 111" />
                     </datalist>
                     <label htmlFor="time">durante</label>
-                    <MinutesAmountInput step={5} min={5} max={60} type="number" name="time" id="time" placeholder="00" />
+                    <MinutesAmountInput
+                        step={5}
+                        min={5}
+                        max={60}
+                        type="number"
+                        name="time"
+                        id="time"
+                        placeholder="00"
+                    />
                     <span>minutos</span>
                 </FormContainer>
                 <CountdownContainer>
@@ -25,7 +52,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled type="submit">
+                <StartCountdownButton disabled={task.length < 3} type="submit">
                     <Play size={24} />
                     Iniciar
                 </StartCountdownButton>
